41604ca29dc83d36741a369e0979ec608c19db04
vlad <vlad5956@gmail.com>
2023-12-08 05:42:26 +0300

    Update server.cpp

--- server.cpp
+++ server.cpp
@@ -91,12 +91,13 @@ int main(int argc, char *argv[])
     {
       port = std::stoi(argv[1]);
       num = std::stoi(argv[2]);
+
+      Server server(port, num);
+      server.Start();
     }
     catch (...)
     {
       std::cout << "Usage: ./best_tcp_server <port> <threads num>" << std::endl;
     }
-    Server server(port, num);
-    server.Start();
   }
 }
